e55764760bec7ae4866bac3890aeeac8ba25d0fd
senorindependiente <pingelchristian@hotmail.com>
2022-02-20 16:52:25 +0100

    Created passport.js file to integrate middleware passport for user authentication
    passport strategies are configured in passport.js file

---
+++ Express/passport.js
@@ -0,0 +1,62 @@
+//integrating middleware passport + http method + jwt method
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
+
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
+
+  //defining the two passport trategies
+
+passport.use(
+    // LocalStrategy defines the basic HTTP authentication for login requests
+  new LocalStrategy(
+    {
+      usernameField: "Username",
+      passwordField: "Password",
+    },
+    (username, password, callback) => {
+      console.log(username + " " + password);
+      Users.findOne({ username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+
+        if (!user) {
+          console.log("incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+
+        console.log("finished");
+        return callback(null, user);
+      });
+    }
+  )
+);
+
+passport.use(
+
+    //JWTStrategy allows to authenticate users based on the JWT submitted alongside their request.
+  new JWTStrategy(
+    {
+        //the JWT is extracted from the header of the HTTP requet. The JWT is called the "bearer token"
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      //secret key to verify the signature of the JWT (verifies that the sender of the JWT is who it says it is)
+      secretOrKey: "your_jwt_secret",
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
