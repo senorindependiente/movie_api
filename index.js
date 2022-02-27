cf8fe9f51ee2a39d1c89e2a2f83aac9ecbf382ec
senorindependiente <pingelchristian@hotmail.com>
2022-02-27 15:56:24 +0100

    debugging

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const { check, validationResult } = require("express-validator");
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
+
 // mongoose.connect("process.env.CONNECTION_URI", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
